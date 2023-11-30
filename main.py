bb2fd650bc4e08ba1495b12c1c858b041c200d02
Grzegorz121979 <pociechagrzegorz@outlook.com>
2023-11-30 17:18:43 +0100

    next commit

--- main.py
+++ main.py
@@ -5,4 +5,23 @@ conn = sqlite3.connect('data.db')
 
 c = conn.cursor()
 
-c.execute("CREATE TABLE users (user text, password text)")
+# c.execute("CREATE TABLE users (user text, password text)")
+
+user_1 = User('Greg', '123')
+user_2 = User('Tom', '456')
+user_3 = User('Sue', '789')
+
+# c.execute("INSERT INTO users VALUES (:name, :password)", {'name': user_1.name, 'password': user_1.password})
+# conn.commit()
+#
+# c.execute("INSERT INTO users VALUES (:name, :password)", {'name': user_2.name, 'password': user_2.password})
+# conn.commit()
+#
+# c.execute("INSERT INTO users VALUES (:name, :password)", {'name': user_3.name, 'password': user_3.password})
+# conn.commit()
+
+c.execute("SELECT * FROM users")
+print(c.fetchall())
+
+conn.commit()
+conn.close()
